cc94d0b9f3cd47b8f566f9f4ff0a15b681a1a1c0
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-10-31 14:14:28 -0700

    Create home method

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,17 @@
 class StaticPagesController < ApplicationController
+  require 'flickr'
+  
+  def home
+    if params[:user_id]
+      flickr = Flickr.new(ENV['FLICKR_API_KEY'], ENV['FLICKR_SHARED_SECRET'])
+
+      @user_id = params[:user_id]
+      @profile = flickr.profile.getProfile(:user_id => @user_id)
+      @photos = flickr.people.getPhotos(:user_id => @user_id)
+      flash.now[:notice] = "User search was successful!"
+    end
+
+    rescue Flickr::FailedResponse
+    flash.now[:alert] = 'Unable to find User ID!'
+  end
 end
